aef64498fed38ac1a8b880a8f81872dcbe28d60a
798817228 <32360880+798817228@users.noreply.github.com>
2017-10-26 10:19:10 +0800

    Update Card.java

--- app/src/main/java/com/example/student/myapplication/Card.java
+++ app/src/main/java/com/example/student/myapplication/Card.java
@@ -17,23 +17,64 @@ public class Card extends FrameLayout {
 
 
 
-    public Card(@NonNull Context context){
+    public Card(@NonNull Context context,int w){
         super(context);
-        FrameLayout.LayoutParams lp=new FrameLayout.LayoutParams(WRAP_CONTENT ,WRAP_CONTENT);
-        lp.setMargins(10,10,0,0);
+        FrameLayout.LayoutParams lp = new FrameLayout.LayoutParams(w-10 ,w-10);
+        lp.setMargins(5,5,5,5);
 
-        View view=new View(context);
+        View view = new View(context);
+        view.setBackgroundColor(0x33ff00ff);
         addView(view,lp);
 
-        tv = new TextView(context);
-        tv.setText(Integer.toString(0));
-        tv.setTextSize(28);
+         tv= new TextView(context);
+       // tv.setText(Integer.toString(0));
+        tv.setTextSize(48);
         tv.setGravity(Gravity.CENTER);
-        addView(tv);
+        addView(tv,lp);
+
     }
     TextView tv;
+    int view;
+     public int getNum()
+    {
+        return value;
+    }
     public void setNum(int num){
-    tv.setText(Integer.toString(num));
+    value = num;
+
+        if (num > 0)
+        tv.setText(Integer.toString(num));
+        else
+            tv.setText("");
+
+
+        switch(num)
+        {
+            case 0:
+                tv.setBackgroundColor(0xffcdc1b4);
+                break;
+
+            case 2:
+                tv.setBackgroundColor(0xffeee4da);
+                tv.setTextColor(0xff000000);
+
+                break;
+            case 4:
+                tv.setBackgroundColor(0xffede0c8);
+                tv.setTextColor(0xff000000);
+
+
+                break;
+            case 8:
+                tv.setBackgroundColor(0xfff2b179);
+                tv.setTextColor(0xffffffff);
+
+                break;
+
+
+        }
+
+     //   tv.setTextColor(0xffffffff);
     }
 
 }
